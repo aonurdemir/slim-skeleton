377382a93764c6833f12d9f14d2fda980e284b93
aonurdemir <aaonurdemir@gmail.com>
2018-01-19 11:23:16 +0300

    Create ResourceServer middleware to intercept request and validate access token.

--- config/middlewares.php
+++ config/middlewares.php
@@ -6,6 +6,19 @@
  * Time: 17:03
  */
 
+use Classes\OAuthServer\Repositories\AccessTokenRepository;
 use Classes\Session\SessionMiddleware;
+use League\OAuth2\Server\Middleware\ResourceServerMiddleware;
 
 //$app->add(new SessionMiddleware($app->getContainer()->get('settings')['session']));
+
+// Init our repositories
+$accessTokenRepository = new AccessTokenRepository(); // instance of AccessTokenRepositoryInterface
+// Path to authorization server's public key
+$publicKeyPath = 'file://' . __DIR__ . '/../public.key';
+// Setup the authorization server
+$server = new \League\OAuth2\Server\ResourceServer(    $accessTokenRepository,    $publicKeyPath);
+
+//$app->add(new ResourceServerMiddleware($server));
+
+
